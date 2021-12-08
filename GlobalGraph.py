621aaa79ee2d7c37d838304bbdc035a99fc7c12d
fengmoon93 <1694565361@qq.com>
2021-12-08 16:25:42 +0800

    Add files via upload

---
+++ GlobalGraph.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/12/6 16:40 
+# @Author : Feng Chenghao
+# @File : GlobalGraph.py 
+# @Software: PyCharm
+from math import gcd
+from typing import List
+import numpy as np
+import torch
+from torch import nn, Tensor
+from layers import Linear
+
+
+def get_agent_idcs(batch):
+    actor_idcs = []
+    count = 0
+    for x in batch:
+        cur_agent_num = len(x['agents'])
+        temp_idcs = torch.arange(count, count + cur_agent_num)
+        actor_idcs.append(temp_idcs)
+        count += cur_agent_num
+    return actor_idcs
+
+
+def get_agent_poses(batch):
+    res = []
+    for x in batch:
+        agent_num = len(x['agents'])
+        locations = np.zeros((agent_num, 2))
+        for i, pos in enumerate(x['agents']):
+            locations[i] = pos[-1]
+        res.append(torch.tensor(locations, dtype=torch.float32))
+    return res
+
+
+class Att(nn.Module):
+    """
+    Attention block to pass context nodes information to target nodes
+    This is used in Actor2Map, Actor2Actor, Map2Actor and Map2Map
+    """
+
+    def __init__(self, n_agt: int, n_ctx: int) -> None:
+        super(Att, self).__init__()
+        norm = "GN"
+        ng = 1
+
+        self.dist = nn.Sequential(
+            nn.Linear(2, n_ctx),
+            nn.ReLU(inplace=True),
+            Linear(n_ctx, n_ctx, norm=norm, ng=ng),
+        )
+
+        self.query = Linear(n_agt, n_ctx, norm=norm, ng=ng)
+
+        self.ctx = nn.Sequential(
+            Linear(3 * n_ctx, n_agt, norm=norm, ng=ng),
+            nn.Linear(n_agt, n_agt, bias=False),
+        )
+
+        self.agt = nn.Linear(n_agt, n_agt, bias=False)
+        self.norm = nn.GroupNorm(gcd(ng, n_agt), n_agt)
+        self.linear = Linear(n_agt, n_agt, norm=norm, ng=ng, act=False)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, agts: Tensor, agt_idcs: List[Tensor], agt_ctrs: List[Tensor], ctx: Tensor, ctx_idcs: List[Tensor],
+                ctx_ctrs: List[Tensor], dist_th: float) -> Tensor:
+        res = agts
+        if len(ctx) == 0:
+            agts = self.agt(agts)
+            agts = self.relu(agts)
+            agts = self.linear(agts)
+            agts += res
+            agts = self.relu(agts)
+            return agts
+
+        batch_size = len(agt_idcs)
+        hi, wi = [], []
+        hi_count, wi_count = 0, 0
+        for i in range(batch_size):
+            dist = agt_ctrs[i].view(-1, 1, 2) - ctx_ctrs[i].view(1, -1, 2)
+            dist = torch.sqrt((dist ** 2).sum(2))
+            mask = dist <= dist_th
+
+            idcs = torch.nonzero(mask, as_tuple=False)
+            if len(idcs) == 0:
+                continue
+
+            hi.append(idcs[:, 0] + hi_count)
+            wi.append(idcs[:, 1] + wi_count)
+            hi_count += len(agt_idcs[i])
+            wi_count += len(ctx_idcs[i])
+        hi = torch.cat(hi, 0)
+        wi = torch.cat(wi, 0)
+
+        agt_ctrs = torch.cat(agt_ctrs, 0)
+        ctx_ctrs = torch.cat(ctx_ctrs, 0)
+        dist = agt_ctrs[hi] - ctx_ctrs[wi]
+        dist = self.dist(dist)
+
+        query = self.query(agts[hi])
+
+        ctx = ctx[wi]
+        ctx = torch.cat((dist, query, ctx), 1)
+        ctx = self.ctx(ctx)
+
+        agts = self.agt(agts)
+        agts.index_add_(0, hi, ctx)
+        agts = self.norm(agts)
+        agts = self.relu(agts)
+
+        agts = self.linear(agts)
+        agts += res
+        agts = self.relu(agts)
+        return agts
+
+
+class A2A(nn.Module):
+    """
+    The actor to actor block performs interactions among actors.
+    """
+
+    def __init__(self, n_actor=128):
+        super(A2A, self).__init__()
+        self.actor2actor_dist = 30
+        att = []
+        for i in range(2):
+            att.append(Att(n_actor, n_actor))
+        self.att = nn.ModuleList(att)
+
+    def forward(self, actors: Tensor, actor_idcs: List[Tensor], actor_ctrs: List[Tensor]) -> Tensor:
+        for i in range(len(self.att)):
+            actors = self.att[i](
+                actors,
+                actor_idcs,
+                actor_ctrs,
+                actors,
+                actor_idcs,
+                actor_ctrs,
+                self.actor2actor_dist
+            )
+        return actors
